e1291679497a9e4ebfc44204408f650a8157e10a
glibjibb <glibjibb@gmail.com>
2017-12-14 11:44:22 -0500

    menu and tutorial scene

---
+++ osana/Assets/HelpBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpBox : MonoBehaviour {
+	
+	[TextArea]
+	public string text;
+	public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			panel.SetActive (true);
+			panel.GetComponentInChildren<Text> ().text = this.text;
+		}
+	}
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") 
+			panel.SetActive (false);
+	}
+}
